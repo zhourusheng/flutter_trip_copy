f4e3091a9115921727d75550bf299b2c873d3bb0
zhourusheng <nfuzrs@126.com>
2019-11-07 20:11:01 +0800

    counter

---
+++ lib/home_page.dart
@@ -0,0 +1,55 @@
+// 导入Material UI组件库
+import 'package:flutter/material.dart';
+
+// HomePage 继承自StatefulWidget类，表示它是一个有状态的组件（Stateful widget）
+class HomePage extends StatefulWidget {
+  HomePage({ Key key, this.title }) : super (key: key);
+  final String title;
+  @override
+  _HomePageState createState() => new _HomePageState();
+}
+
+
+class _HomePageState extends State<HomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+     _counter++; 
+    });
+  }
+
+  @override
+  // Flutter框架会调用Widget的build方法来构建widget树，最终将widget树渲染到设备屏幕上
+  Widget build(BuildContext context) {
+
+    /***
+     * Scaffold 是 Material组件库中提供的一个组件，它提供了默认的导航栏、标题和包含主屏幕widget树（后同“组件树”或“部件树”）的body属性。组件树可以很复杂。
+     * **/ 
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(
+              'You have pushed the button this many times:',
+            ),
+            new Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: new Icon(Icons.add),
+      ),
+    );
+  }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,28 @@
+// 导入Material UI组件库
 import 'package:flutter/material.dart';
-import 'navigator/tab_navigator.dart';
 
+// 导入navigator
+// import 'navigator/tab_navigator.dart';
+
+import 'home_page.dart';
+
+// 应用的入口
 void main() => runApp(MyApp());
 
+// MyApp类代表Flutter应用，它继承了 StatelessWidget类，这也就意味着应用本身也是一个widget。
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // 应用名称
       title: '携程App',
       theme: ThemeData(
+        // 蓝色主题
         primarySwatch: Colors.blue
       ),
-      home: TabNavigator(),
+      // 应用首页路由
+      // home: TabNavigator(),
+      home: HomePage(title: 'hahaha'),
     );
   }
 }
